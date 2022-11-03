c7017a47d3cac89d016a4293a4f20b37a15c052a
mstevenson <mbstevenson96@gmail.com>
2022-11-03 19:51:18 -0400

    complete part 1

--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
-# Add the Cat class & list and view function below the imports
-class Finch:  # Note that parens are optional if not inheriting from another class
+class Finch:
   def __init__(self, name, breed, description, age):
     self.name = name
     self.breed = breed
