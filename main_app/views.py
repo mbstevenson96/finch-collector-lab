c43be3b0de50cd142deec984722ba3bac4e9f1c1
mstevenson <mbstevenson96@gmail.com>
2022-11-06 18:17:48 -0500

    add admin feature and home page

--- main_app/views.py
+++ main_app/views.py
@@ -1,28 +1,16 @@
 from django.shortcuts import render
+from .models import Finch
 
 # Add the following import
 from django.http import HttpResponse
 
-class Finch:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Carl', 'Tropical Borneo', 'Likes Halloween', 3),
-  Finch('Peggy', 'Finch', 'Likes Art', 0),
-  Finch('Freddy', 'Finch', 'Happy fluff ball.', 4),
-  Finch('Susan', 'Zebra Finch', 'Likes to People Watch', 6)
-]
-
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello Birdies!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', { 'finches': finches })
